a8c940c02e90f6d85fa36de56fa0ed8f4c1ccf78
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-12-31 11:22:32 +0100

    Some improvements

--- src/make.jl
+++ src/make.jl
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
 using Weave
 
-source = filter(s->endswith(s, ".jmd"), readdir(@__DIR__()))
-target = joinpath.(@__DIR__(), source)
+const BUILDDIR = joinpath(@__DIR__(), "_build")
 
-weave.(target;
+function weaveall()
+    source = filter(s->endswith(s, ".jmd"), readdir(@__DIR__()))
+    target = joinpath.(@__DIR__(), source)
+    
+    @info "Weaving all documents..."
+    weave.(target;
     doctype  = "md2html",
-    out_path = joinpath(@__DIR__(), "_build/"),
+    out_path = BUILDDIR,
     template = joinpath(@__DIR__(), "template.html"))
+    return nothing;
+end
+
+function cleanall()
+    @info "Removing cache directories..."
+    source = filter(s->startswith(s, "jl_"), readdir(BUILDDIR))
+    target = joinpath.(BUILDDIR, source)
+    rm.(target; recursive=true, force=true)
+    return nothing;
+end
+
+weaveall()
+cleanall()
