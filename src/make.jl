4559c71226d0e024ee961d246dce687044d60092
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-12-31 10:55:51 +0100

    Fixing path

--- src/make.jl
+++ src/make.jl
@@ -2,7 +2,9 @@
 using Weave
 
 source = filter(s->endswith(s, ".jmd"), readdir(@__DIR__()))
+target = joinpath.(@__DIR__(), source)
 
-weave.(source;
+weave.(target;
     doctype  = "md2html",
-    out_path = joinpath(@__DIR__(), "_build/"))
+    out_path = joinpath(@__DIR__(), "_build/"),
+    template = joinpath(@__DIR__(), "template.html"))
