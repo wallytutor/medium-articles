3e14a6789d3e7d70d42a1ffc18acafccfb601f5e
wallytutor <walter.dalmazsilva@gmail.com>
2024-01-11 19:42:06 +0000

    Deploying to gh-pages from  @ a5260e35e97d95bbcb6dfe2fdf930c21ba640a8d 🚀

---
+++ assets/content/transport-phenomena/001-advection-1d/code/output.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+kymograph(p2; xticks = 0.0:0.5:ustrip(L), # hide
+              yticks = 0.0:0.2:ustrip(T), # hide
+              zticks = 0.5:0.1:1.0) # hide
