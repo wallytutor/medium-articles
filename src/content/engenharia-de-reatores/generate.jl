6721c8037e0d9bb8f173a4dcd40716e6abd9f229
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-01-13 22:18:18 +0100

    Checking automated conversion

--- src/content/engenharia-de-reatores/generate.jl
+++ src/content/engenharia-de-reatores/generate.jl
@@ -3,40 +3,37 @@ using JuliaFormatter
 using Pluto: ServerSession, SessionActions
 using Pluto: generate_html
 
-function convertnotebook(s::ServerSession, nbname::String)
-    nbpath = joinpath(@__DIR__, "$(nbname).jl")
-    pgpath = "$(nbname).html"
-    
-    if !format_file(nbpath)
-        @error "file not formatted: $(nbpath)"
-    end
-    
-    if isfile(pgpath) && !FORCE
-        VERBOSE > 2 && @info "file exists: $(pgpath)"
-        return
-    end
-    
-    VERBOSE > 0 && @info "working on $(nbname)"
-    nb = SessionActions.open(s, nbpath; run_async=false)
-    write(pgpath, generate_html(nb))
-    SessionActions.shutdown(s, nb)
-end
-
 function workflow(nblist::Vector{String})
     s = ServerSession()
     s.options.server.launch_browser = false
 
     for nbname in nblist
-        convertnotebook(s, nbname)
+        nbpath = joinpath(@__DIR__, "$(nbname).jl")
+        pgpath = joinpath(@__DIR__, "$(nbname).html")
+        
+        if !format_file(nbpath)
+            @error "file not formatted: $(nbpath)"
+        end
+        
+        if isfile(pgpath) && !FORCE
+            VERBOSE > 2 && @info "file exists: $(pgpath)"
+            return
+        end
+        
+        VERBOSE > 0 && @info "working on $(nbname)"
+        nb = SessionActions.open(s, nbpath; run_async=false)
+        write(pgpath, generate_html(nb))
+        SessionActions.shutdown(s, nb)
     end
 end
 
 VERBOSE = 3
 
-FORCE = false
+FORCE = true
 
 notebooks_ready = [
     "001-reator-pistao"
+    # "002-reator-pistao"
 ]
 
 workflow(notebooks_ready)
