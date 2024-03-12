27aee481d676113bfc7e4ed645e0ef7d776b5d49
wallytutor <walter.dalmazsilva@gmail.com>
2024-03-12 18:20:58 +0000

    Deploying to gh-pages from  @ 9d8991b598380c04e2989450b614b3903e278c9e 🚀

--- content/transport-phenomena/basilisk/01-Tutorial-Basics/bump.c
+++ content/transport-phenomena/basilisk/01-Tutorial-Basics/bump.c
@@ -1,4 +1,12 @@
+// Change type of grid used by the solver (~2x faster than Quadtree).
+// But it cannot be used with adaptive grid refinement.
+// #include "grid/cartesian.h"
+
+// Add Saint-Venant equation solver.
 #include "saint-venant.h"
+
+// Refinement level to use in simulation.
+#define LEVEL 8
 
 // Specify wave height `h` at starting time `t = 0`.
 event init (t = 0) {
@@ -9,9 +17,28 @@ event init (t = 0) {
 }
 
 // Generate images of results every iteration.
-// By default redirected to `stdout`.
-event images (i++) {
-    output_ppm(h);
+//event images (i++) {          // By iteration number.
+event images (t += 4.0/300.0) { // By physical time.
+    // By default redirected to `stdout`.
+    // Add interpolation for grid refinement display.
+    output_ppm(h, linear = true);
+
+    // Store animation of grid additionally in a file.
+    scalar l[];
+    foreach() {
+        l[] = level;
+    }
+
+    // Static variable is allocated only in the first call here.
+    static FILE * fp = fopen("grid.ppm", "w");
+
+    // Min and max ensure constant color scale across animation.
+    output_ppm(l, fp, min = 0, max = LEVEL);
+}
+
+// Adaptive grid refinement.
+event adapt (i++) {
+    adapt_wavelet({h}, (double []){4.0e-03}, maxlevel = LEVEL);
 }
 
 // Print statistics of `h` every step to `stderr`.
@@ -21,8 +48,9 @@ event graphs (i++) {
     fprintf(stderr, "%g %g %g\n", t, s.min, s.max);
 }
 
-// At iteration `i = 10` (end of calculation), show integration time `t`.
-event end (i = 1200) {
+// At iteration `i` (end of calculation), show integration time `t`.
+// event end (i = 1200) { // By iteration number.
+event end (t = 4) {       // By physical time.
     printf("i = %d t = %g\n", i, t);
 }
 
@@ -31,7 +59,7 @@ int main() {
     origin(-0.5, -0.5);
 
     // Increase grid resolution.
-    init_grid(256);
+    init_grid(1 << LEVEL);
 
     // Run simulation.
     run();
