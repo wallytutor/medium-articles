a5856a12dcc6eb85277c22b9e5ebb557773e5a5e
wallytutor <walter.dalmazsilva@gmail.com>
2024-03-12 16:55:54 +0000

    Deploying to gh-pages from  @ 0f7f66091a7463aaa47183df7e33a6e5984fb518 🚀

--- content/transport-phenomena/basilisk/01-Tutorial-Basics/bump.c
+++ content/transport-phenomena/basilisk/01-Tutorial-Basics/bump.c
@@ -1,14 +1,37 @@
 #include "saint-venant.h"
 
-// At iteration `i = 10` (end of calculation), show integration time `t`.
-event end (i = 10) {
-    printf("i = %d t = %g\n", i, t);
+// Specify wave height `h` at starting time `t = 0`.
+event init (t = 0) {
+    double a = 1.0, b = 200.0;
+    foreach() {
+        h[] = 0.1 + a * exp(-b * (x*x + y*y));
+    }
 }
 
+// Generate images of results every iteration.
+// By default redirected to `stdout`.
+event images (i++) {
+    output_ppm(h);
+}
+
+// Print statistics of `h` every step to `stderr`.
+// Notice `stdout` is taken by the `images` event.
+event graphs (i++) {
+    stats s = statsf(h);
+    fprintf(stderr, "%g %g %g\n", t, s.min, s.max);
+}
+
+// At iteration `i = 10` (end of calculation), show integration time `t`.
+event end (i = 1200) {
+    printf("i = %d t = %g\n", i, t);
+}
 
 int main() {
     // Change origin of coordinate system.
     origin(-0.5, -0.5);
+
+    // Increase grid resolution.
+    init_grid(256);
 
     // Run simulation.
     run();
