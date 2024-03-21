80c42fe729a85aa75cd0d5b66ba99b3caaa5b796
wallytutor <walter.dalmazsilva@gmail.com>
2024-03-21 06:59:09 +0000

    Deploying to gh-pages from  @ 1e526739856723f6e98cf46a71d15cb927452934 🚀

--- content/transport-phenomena/basilisk/08-Vortex-Street/project-post.h
+++ content/transport-phenomena/basilisk/08-Vortex-Street/project-post.h
@@ -1,2 +1,22 @@
 #pragma once
 
+event logfile (i++)
+{
+    fprintf(stderr, "%d %g %d %d\n", i, t, mgp.i, mgu.i);
+}
+
+event movies (i += 4; t <= 15.0)
+{
+    scalar omega[], m[];
+    vorticity (u, omega);
+    foreach()
+    {
+        m[] = cs[] - 0.5;
+    }
+       
+    output_ppm (omega, file = "vort.mp4", box = {{-0.5,-0.5},{7.5,0.5}},
+                min = -10, max = 10, linear = true, mask = m);
+    output_ppm (f, file = "f.mp4", box = {{-0.5,-0.5},{7.5,0.5}},
+                linear = false, min = 0, max = 1, mask = m);
+}
+
